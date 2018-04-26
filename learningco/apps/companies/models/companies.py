194a414a2547d81400587d7ef46ce13fb4737769
Luis Herrada <luisehk@gmail.com>
2018-04-26 17:04:09 -0500

    Company model: add industry, size, human_resources and leaders fields

--- learningco/apps/companies/models/companies.py
+++ learningco/apps/companies/models/companies.py
@@ -1,5 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from .industries import Industry
+
+
+User = get_user_model()
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=150)
+    COMPANY_SIZE_UNKNOWN = '0'
+    COMPANY_SIZE_SMALL = 'A'
+    COMPANY_SIZE_MEDIUM = 'B'
+    COMPANY_SIZE_LARGE = 'C'
+    COMPANY_SIZES = (
+        (COMPANY_SIZE_UNKNOWN, 'No especificado'),
+        (COMPANY_SIZE_SMALL, '50-500 empleados'),
+        (COMPANY_SIZE_MEDIUM, '500-5,000 empleados'),
+        (COMPANY_SIZE_LARGE, '+5,000 empleados'),
+    )
+
+    name = models.CharField(
+        max_length=150)
+    industry = models.ForeignKey(
+        Industry, on_delete=models.SET_NULL, null=True)
+    size = models.CharField(
+        max_length=1, choices=COMPANY_SIZES, default=COMPANY_SIZE_UNKNOWN)
+    human_resources = models.ManyToManyField(
+        User, related_name='hr_companies')
+    leaders = models.ManyToManyField(
+        User, related_name='leader_companies')
