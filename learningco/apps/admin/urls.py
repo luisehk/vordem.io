e9febab343e0750e014bde2e93135945de7ca750
Luis Herrada <luisehk@gmail.com>
2018-04-27 10:11:40 -0500

    Company: add create, update and delete views

--- learningco/apps/admin/urls.py
+++ learningco/apps/admin/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
-from .views.companies import CompanyList
+from .views.companies import (
+    CompanyList, CompanyCreate, CompanyUpdate, CompanyDelete
+)
 
 urlpatterns = [
-    url(r'^companies$', CompanyList.as_view(), name="company_list"),
+    url(r'^companies$',
+        CompanyList.as_view(), name="company-list"),
+    url(r'^companies/add/$',
+        CompanyCreate.as_view(), name='company-add'),
+    url(r'^companies/(?P<pk>[0-9]+)/$',
+        CompanyUpdate.as_view(), name='company-update'),
+    url(r'^companies/(?P<pk>[0-9]+)/delete/$',
+        CompanyDelete.as_view(), name='company-delete'),
 ]
--- learningco/apps/admin/views/companies.py
+++ learningco/apps/admin/views/companies.py
@@ -1,9 +1,28 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
-    ListView)
+    ListView, CreateView, UpdateView, DeleteView)
+from django.urls import reverse_lazy
 from ...companies.models import Company
 
 
 class CompanyList(LoginRequiredMixin, ListView):
     model = Company
     template_name = 'admin/company_list.html'
+
+
+class CompanyCreate(LoginRequiredMixin, CreateView):
+    model = Company
+    fields = ['name']
+    template_name = 'admin/company_create.html'
+
+
+class CompanyUpdate(LoginRequiredMixin, UpdateView):
+    model = Company
+    fields = ['name']
+    template_name = 'admin/company_update.html'
+
+
+class CompanyDelete(LoginRequiredMixin, DeleteView):
+    model = Company
+    template_name = 'admin/company_delete.html'
+    success_url = reverse_lazy('company-list')
