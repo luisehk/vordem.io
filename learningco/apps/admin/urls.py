6b44dba83f6da444fd470cc32b18fadfdb87a19a
Luis Herrada <luisehk@gmail.com>
2018-05-17 10:55:59 -0500

    Add temporary views and templates for new mockups

--- learningco/apps/admin/urls.py
+++ learningco/apps/admin/urls.py
@@ -7,6 +7,11 @@ from .views.companies import (
 from .views.industries import (
     IndustryCreate, IndustryUpdate, IndustryDelete
 )
+from .views.content import (
+    Content01, Content02, Content03, Content04, Content05, Content06,
+    Content07, Content08, Content09, Content10, Content11, Content12,
+    Content13,
+)
 from .views.admin import IndexView
 
 urlpatterns = [
@@ -33,6 +38,20 @@ urlpatterns = [
         IndustryUpdate.as_view(), name='industry-update'),
     url(r'^industries/(?P<pk>[0-9]+)/delete/$',
         IndustryDelete.as_view(), name='industry-delete'),
+
+    url(r'content/01$', Content01.as_view(), name='content-01'),
+    url(r'content/02$', Content02.as_view(), name='content-02'),
+    url(r'content/03$', Content03.as_view(), name='content-03'),
+    url(r'content/04$', Content04.as_view(), name='content-04'),
+    url(r'content/05$', Content05.as_view(), name='content-05'),
+    url(r'content/06$', Content06.as_view(), name='content-06'),
+    url(r'content/07$', Content07.as_view(), name='content-07'),
+    url(r'content/08$', Content08.as_view(), name='content-08'),
+    url(r'content/09$', Content09.as_view(), name='content-09'),
+    url(r'content/10$', Content10.as_view(), name='content-10'),
+    url(r'content/11$', Content11.as_view(), name='content-11'),
+    url(r'content/12$', Content12.as_view(), name='content-12'),
+    url(r'content/13$', Content13.as_view(), name='content-13'),
 
     url(r'^$', IndexView.as_view(), name='index'),
 ]
