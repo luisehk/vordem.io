95fd027e9c9c8457e7ed4b40dcdd3a7cc53900f7
Luis Herrada <luisehk@gmail.com>
2018-05-29 13:59:13 -0500

    Add lesson model

--- learningco/apps/content/models/lessons.py
+++ learningco/apps/content/models/lessons.py
@@ -15,3 +15,20 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Lesson(models.Model):
+    skill = models.ForeignKey(
+        Skill, verbose_name='Competencia',
+        null=True, blank=True,
+        on_delete=models.CASCADE)
+    name = models.CharField(
+        verbose_name='Nombre',
+        max_length=150)
+    default = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
