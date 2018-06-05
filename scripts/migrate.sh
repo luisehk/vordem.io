ea1bc12faf36a3200c9a9649a8e440266863add7
Luis Herrada <luisehk@gmail.com>
2018-06-04 21:43:15 -0500

    Bash: prevent word splitting

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,7 +4,7 @@ set -x
 
 DC_FILE=${1:-docker-compose.yml}
 
-docker-compose -f $DC_FILE run app python manage.py migrate contenttypes
-docker-compose -f $DC_FILE run app python manage.py migrate auth
-docker-compose -f $DC_FILE run app python manage.py migrate sites
-docker-compose -f $DC_FILE run app python manage.py migrate
+docker-compose -f "$DC_FILE" run app python manage.py migrate contenttypes
+docker-compose -f "$DC_FILE" run app python manage.py migrate auth
+docker-compose -f "$DC_FILE" run app python manage.py migrate sites
+docker-compose -f "$DC_FILE" run app python manage.py migrate
