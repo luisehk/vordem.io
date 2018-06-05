bee4bcc33e8e9139f8c1932c74264ad05222bf15
Luis Herrada <luisehk@gmail.com>
2018-06-04 23:26:15 -0500

    Coverage in scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,5 +9,6 @@ docker-compose -f "$DC_FILE" run app flake8
 docker-compose -f "$DC_FILE" run app radon cc "$CODE_PATH"
 docker-compose -f "$DC_FILE" run app radon mi "$CODE_PATH"
 docker-compose -f "$DC_FILE" run app bandit -r "$CODE_PATH"
-docker-compose -f "$DC_FILE" run app coverage run --source="CODE_PATH" manage.py test
+docker-compose -f "$DC_FILE" run app coverage run --source="$CODE_PATH" manage.py test
+docker-compose -f "$DC_FILE" run app coverage report -m --fail-under=80
 # docker-compose -f $DC_FILE run app python manage.py test -k
