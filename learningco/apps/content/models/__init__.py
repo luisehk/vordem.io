d213a2c23a4de7fa655d2558c77b6ef146282ac1
Luis Herrada <luisehk@gmail.com>
2018-06-04 21:10:28 -0500

    Add Quiz completion models

--- learningco/apps/content/models/__init__.py
+++ learningco/apps/content/models/__init__.py
@@ -1,3 +1,5 @@
-from .lessons import Lesson  # noqa
+from .lessons import (  # noqa
+    Lesson, Intro, Video, Quiz, Question, Option,  # noqa
+    Article, ActivityList, Activity)  # noqa
 from .skills import Skill  # noqa
 from .bundles import Bundle  # noqa
--- learningco/apps/progress/models/leaders.py
+++ learningco/apps/progress/models/leaders.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from ...content.models import Skill, Lesson
+from ...content.models import Skill, Lesson, Question, Option
 from django.db import models
 
 
@@ -15,6 +15,25 @@ class LessonCompletion(models.Model):
         on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
     score = models.IntegerField(default=0)
+
+
+class QuizCompletion(LessonCompletion):
+    quiz_score = models.IntegerField(default=0)
+
+
+class QuizAnswer(models.Model):
+    quiz_completion = models.ForeignKey(
+        QuizCompletion,
+        on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE)
+    option = models.ForeignKey(
+        Option,
+        on_delete=models.CASCADE)
+    leader = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE)
 
 
 class SkillCompletion(models.Model):
