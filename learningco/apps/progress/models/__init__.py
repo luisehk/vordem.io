04b8f48eadca74f2f2a4052ce5ac3a7ce966200f
Luis Herrada <luisehk@gmail.com>
2018-06-04 21:00:11 -0500

    LessonCompletion model

---
+++ learningco/apps/progress/models/__init__.py
@@ -0,0 +1,1 @@
+from .leaders import LessonCompletion  # noqa
--- learningco/apps/progress/models/leaders.py
+++ learningco/apps/progress/models/leaders.py
@@ -1,8 +1,17 @@
 from django.contrib.auth import get_user_model
-from ...companies.models import Company
-from ...content.models import Skill
+from ...content.models import Skill, Lesson
 from django.db import models
 
 
 User = get_user_model()
 
+
+class LessonCompletion(models.Model):
+    lesson_id = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE)
+    leader_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
