5b70240d4f56bb524269a73e7d123531fc53d0c4
Luis Herrada <luisehk@gmail.com>
2018-06-10 01:55:12 -0500

    Cleanup content mixins

--- learningco/apps/content/views/mixins.py
+++ learningco/apps/content/views/mixins.py
@@ -1,25 +1,20 @@
 from django.urls import reverse_lazy
-from ...utils.mixins import EditAfterSuccess
 from ...content.models import Video
 
 
 DEFAULT_LESSON_FIELDS = ['skill', 'name', 'body', 'default', 'thumbnail']
 
 
-class VideoGenericView(object):
-    model = Video
-
+class LessonGenericView(object):
     def get_success_url(self):
-        return reverse_lazy('admin:skill-detail', args=(self.object.skill.id,))
+        return reverse_lazy(
+            'admin:skill-detail',
+            args=(self.object.skill.id,))
+
+
+class VideoGenericView(LessonGenericView):
+    model = Video
 
 
 class VideoFormView(VideoGenericView):
     fields = ['skill', 'name', 'body', 'default', 'video_url']
-
-
-class EditVideoAfterSuccess(EditAfterSuccess):
-    edit_reversable_url = 'content:video-update'
-
-
-class EditIndustryAfterSuccess(EditAfterSuccess):
-    edit_reversable_url = 'content:industry-update'
