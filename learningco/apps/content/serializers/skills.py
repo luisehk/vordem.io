ffb68c550272b5b00540e6c64ebf658f4e7a3b17
Luis Herrada <luisehk@gmail.com>
2018-06-11 10:33:57 -0500

    Return intro, video and article in properties instead of array

--- learningco/apps/content/serializers/skills.py
+++ learningco/apps/content/serializers/skills.py
@@ -1,13 +1,33 @@
 from rest_framework import serializers
-from ..models import Skill
-from .content import LessonPolymorphicSerializer
+from ..models import Skill, Intro, Video, Article
+from .content import IntroSerializer, VideoSerializer, ArticleSerializer
 
 
 class SkillSerializer(serializers.ModelSerializer):
-    lessons = LessonPolymorphicSerializer(many=True)
+    intro = serializers.SerializerMethodField()
+    video = serializers.SerializerMethodField()
+    article = serializers.SerializerMethodField()
 
     class Meta:
         model = Skill
         fields = [
-            'name', 'lessons'
+            'name', 'intro', 'video', 'article'
         ]
+
+    def get_lesson(self, obj, model_class, serializer_class):
+        data = model_class.objects.filter(skill=obj).last()
+
+        if data:
+            serializer = serializer_class(data)
+            return serializer.data
+        else:
+            return None
+
+    def get_intro(self, obj):
+        return self.get_lesson(obj, Intro, IntroSerializer)
+
+    def get_video(self, obj):
+        return self.get_lesson(obj, Video, VideoSerializer)
+
+    def get_article(self, obj):
+        return self.get_lesson(obj, Article, ArticleSerializer)
