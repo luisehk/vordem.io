906c095f58001acc6c3cd041f9f4d0302041e0e0
Luis Herrada <luisehk@gmail.com>
2018-07-11 23:39:54 -0500

    Show company score

--- learningco/apps/companies/serializers/companies.py
+++ learningco/apps/companies/serializers/companies.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from ..models import Company
 from .industries import IndustrySerializer
+from ...score.serializers.scores import CompanyScoreSerializer
 
 
 class CompanySerializer(serializers.ModelSerializer):
     industry = IndustrySerializer()
     size = serializers.SerializerMethodField()
+    company_score = CompanyScoreSerializer(read_only=True)
 
     class Meta:
         model = Company
-        fields = ['id', 'name', 'industry', 'size']
+        fields = ['id', 'name', 'industry', 'size', 'company_score']
 
     def get_size(self, obj):
         return obj.get_size_display()
