bcd08c0ee9a957a6c76abcfe9084e49068ddda99
Luis Herrada <luisehk@gmail.com>
2018-07-13 03:12:04 -0500

    Add leaders views (without templates yet)

--- learningco/apps/admin/urls.py
+++ learningco/apps/admin/urls.py
@@ -5,20 +5,18 @@ from .views.rest.companies import (
     AddHumanResourcesToCompany, AddLeaderToCompany)
 from .views.web.companies import (
     CompanyCreate, CompanyUpdate, CompanyDelete,
-    CompanyDetail, CompanyList, CompanyLeaders
-)
+    CompanyDetail, CompanyList, CompanyLeaders)
+from .views.web.leaders import (
+    LeaderUpdate, LeaderDelete, LeaderDetail)
 from .views.web.industries import (
     IndustryCreate, IndustryUpdate, IndustryDelete, IndustryList,
-    IndustryDetail
-)
+    IndustryDetail)
 from .views.web.skills import (
-    SkillCreate, SkillUpdate, SkillDelete, SkillList, SkillDetail
-)
+    SkillCreate, SkillUpdate, SkillDelete, SkillList, SkillDetail)
 from .views.web.content import (
     Content01, Content02, Content03, Content04, Content05, Content06,
     Content07, Content08, Content09, Content10, Content11, Content12,
-    Content13,
-)
+    Content13,)
 
 urlpatterns = [
     url(r'^companies$',
@@ -48,6 +46,13 @@ urlpatterns = [
         IndustryUpdate.as_view(), name='industry-update'),
     url(r'^industries/(?P<pk>[0-9]+)/delete/$',
         IndustryDelete.as_view(), name='industry-delete'),
+
+    url(r'^leaders/detail/(?P<pk>[0-9]+)/$',
+        LeaderDetail.as_view(), name='leader-detail'),
+    url(r'^leaders/(?P<pk>[0-9]+)/$',
+        LeaderUpdate.as_view(), name='leader-update'),
+    url(r'^leaders/(?P<pk>[0-9]+)/delete/$',
+        LeaderDelete.as_view(), name='leader-delete'),
 
     url(r'^skills$',
         SkillList.as_view(), name="skill-list"),
---
+++ learningco/apps/admin/views/web/leaders.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    UpdateView, DeleteView, DetailView)
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class LeaderGenericView(object):
+    model = User
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'admin:leader-detail',
+            args=(self.object.id,))
+
+
+class LeaderFormView(LeaderGenericView):
+    fields = ['first_name', 'last_name']
+
+
+class LeaderUpdate(LoginRequiredMixin, LeaderFormView, UpdateView):
+    template_name = 'admin/leaders/update.html'
+
+
+class LeaderDelete(LoginRequiredMixin, LeaderGenericView, DeleteView):
+    template_name = 'admin/leaders/delete.html'
+
+
+class LeaderDetail(LoginRequiredMixin, DetailView):
+    template_name = 'admin/leaders/detail.html'
+    queryset = User.objects.all()
