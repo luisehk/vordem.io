617ebcb94314c4d6c74a691018846f981b8f1aeb
Luis Herrada <luisehk@gmail.com>
2018-07-13 04:41:30 -0500

    Allow to update others people profile picture

--- learningco/apps/users/api/views.py
+++ learningco/apps/users/api/views.py
@@ -11,18 +11,10 @@ class ProfileViewSet(ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
-    def get_queryset(self):
-        u = self.request.user
-        return super().get_queryset().filter(user=u)
-
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    def get_queryset(self):
-        u = self.request.user.id
-        return super().get_queryset().filter(id=u)
 
 
 class MyselfView(APIView):
