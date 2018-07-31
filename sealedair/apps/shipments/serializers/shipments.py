69f505670d69b3372ee99cfb7092792cc86aac79
Luis Herrada <luisehk@gmail.com>
2018-07-30 22:14:56 -0500

    Shipments API: show statuses, comments and users

--- sealedair/apps/shipments/serializers/shipments.py
+++ sealedair/apps/shipments/serializers/shipments.py
@@ -1,12 +1,37 @@
 from rest_framework import serializers
-from ..models import Shipment
+from ..models import Shipment, Comment, Status
+from ...users.api.serializers import UserSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Comment
+        fields = [
+            'user', 'datetime', 'body'
+        ]
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = [
+            'checkpoint', 'time_status',
+            'start_datetime', 'end_datetime'
+        ]
 
 
 class ShipmentSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True)
+    status_history = StatusSerializer(many=True)
+    current_status = StatusSerializer()
+
     class Meta:
         model = Shipment
         fields = [
             'carrier', 'plant', 'code', 'current_status',
             'start_datetime', 'arrival_datetime',
-            'estimated_arrival_datetime', 'delay_reason'
+            'estimated_arrival_datetime', 'delay_reason',
+            'comments', 'status_history'
         ]
