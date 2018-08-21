aa594d8b3ccb355d50d1473df203d42c625a7c7a
Luis Herrada <luisehk@gmail.com>
2018-08-20 23:57:53 -0500

    Modal: successfully create shipments

--- static/js/vue/shipments/create-shipment.js
+++ static/js/vue/shipments/create-shipment.js
@@ -22,5 +22,65 @@ var newShipmentApp = new Vue({
       {value: 2, text: 'CRE'},
     ],
     loading: false
+  },
+  computed: {
+    formIsValid: function () {
+      return this.shipment.code &&
+        this.shipment.plant &&
+        this.shipment.truck.code &&
+        this.shipment.truck.carrier &&
+        true;
+    }
+  },
+  methods: {
+    submit: function() {
+      var self = this;
+
+      this.loading = true;
+
+      $.ajax({
+        url: '/shipments/api/shipments/',
+        type: 'POST',
+        data: JSON.stringify(this.shipment),
+        dataType: 'json',
+        contentType: 'application/json',
+        headers: {
+          'X-CSRFToken': getCookie('csrftoken')
+        },
+        success : function(json) {
+          self.loading = false;
+          self.success.call(self, json);
+        },
+        error : function(xhr, status, error) {
+          self.loading = false;
+          self.success.call(self, xhr, status, error);
+        }
+      });
+    },
+
+    success: function(json) {
+      // reset values
+      this.shipment.code = '';
+      this.shipment.truck.code = '';
+
+      // close modal
+      $(this.$el).modal('toggle')
+
+      // notification
+      $.gritter.add({
+        title: 'Éxito',
+        text: 'El embarque #' + json.code + ' ha sido creado exitosamente.',
+        class_name: 'color success'
+      });
+    },
+
+    error: function(xhr, status, error) {
+      // notification
+      $.gritter.add({
+        title: 'Éxito',
+        text: 'Hubo un error al crear el embarque.',
+        class_name: 'color danger'
+      });
+    }
   }
 });
