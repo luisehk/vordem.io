00c2de20794c3b27897458849570f1e753901c5a
Luis Herrada <luisehk@gmail.com>
2018-08-20 22:57:46 -0500

    Retrieve select values from vue

--- static/js/vue/shipments/create-shipment.js
+++ static/js/vue/shipments/create-shipment.js
@@ -1,4 +1,26 @@
 var newShipmentApp = new Vue({
   el: '#new-shipment',
-  delimiters: ['${', '}']
+  delimiters: ['${', '}'],
+  data: {
+    shipment: {
+      code: '',
+      truck: {
+        code: '',
+        carrier: 1
+      },
+      plant: 1
+    },
+    plants: [
+      {value: 1, text: 'Aurora, CO'},
+      {value: 2, text: 'Duncan, SC'},
+      {value: 3, text: 'Iowa Park, TX'},
+      {value: 4, text: 'Saint Joe, MO'},
+      {value: 5, text: 'Sturtevant, WI'},
+    ],
+    carriers: [
+      {value: 1, text: 'KLLM'},
+      {value: 2, text: 'CRE'},
+    ],
+    loading: false
+  }
 });
