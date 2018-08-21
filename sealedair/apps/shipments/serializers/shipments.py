10b78686c99873b3aca6aaf7ac481e7df46f7f3e
Luis Herrada <luisehk@gmail.com>
2018-08-20 22:18:13 -0500

    pep8

--- sealedair/apps/shipments/serializers/shipments.py
+++ sealedair/apps/shipments/serializers/shipments.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+from django.db import transaction
 from ..models import Shipment, Comment, Status
 from ...users.api.serializers import UserSerializer
 from ...providers.serializers.carriers import (
     TruckSerializer, TruckCreationSerializer)
 from ...company.serializers.plants import PlantSerializer
 from ...providers.models import Truck
+
 
 class CommentSerializer(serializers.ModelSerializer):
     user = UserSerializer()
