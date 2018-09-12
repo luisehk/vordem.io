f5f709db7ef491620f385049deb4779ac702f5af
Luis Herrada <luisehk@gmail.com>
2018-09-12 09:15:03 -0500

    load metrics using ajax

--- static/js/vue/shipments/dashboard.js
+++ static/js/vue/shipments/dashboard.js
@@ -142,5 +142,54 @@ Vue.component('shipments-column', {
 
 var app = new Vue({
   el: '#shipments-dashboard',
-  delimiters: ['${', '}']
+  delimiters: ['${', '}'],
+  data: {
+    plants: []
+  },
+  created: function() {
+    this.loadPlantsMetricsInLoop();
+  },
+  methods: {
+    _get: function(url, success, error) {
+      $.ajax({
+        url: url,
+        type: 'GET',
+        success : success,
+        error : error
+      });
+    },
+
+
+    loadPlantsMetricsInLoop: function() {
+      var self = this;
+
+      // load it the first time
+      this.loadPlantsMetrics();
+
+      // load every 5 seconds
+      setInterval(function() {
+        self.loadPlantsMetrics.call(self);
+      }, 5000);
+    },
+
+    loadPlantsMetrics: function() {
+      var self = this;
+
+      this._get(
+        '/shipments/shipments/metrics-per-plant/',
+        function(json) {
+          self.plants = json;
+        },
+        function(xhr, textStatus, errorThrown ) {
+          console.error('error loading data');
+        }
+      );
+    },
+
+    getPlantStyle: function(plant) {
+      return {
+        'border-top-color': plant.color
+      };
+    }
+  }
 });
