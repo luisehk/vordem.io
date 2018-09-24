f70dfea125874ccbfc1072f69775d6047bfa17cc
Luis Herrada <luisehk@gmail.com>
2018-09-24 13:05:57 -0500

    show eta in cards

--- static/js/vue/shipments/card.js
+++ static/js/vue/shipments/card.js
@@ -51,6 +51,56 @@ Vue.component('shipment-card', {
       window.updateShipmentApp.open(
         JSON.parse(JSON.stringify(this.shipment))
       );
-    }
+    },
+
+    _formatDatetime: function(datetime) {
+      if(datetime) {
+        return this._formatDateWithoutYear(datetime) +
+          ', ' +
+          this._formatTime(datetime);
+      } else {
+        return '';
+      }
+    },
+
+    _formatDate: function(date) {
+      var date = new Date(date);
+      var monthNames = [
+        "Ene", "Feb", "Mar",
+        "Abr", "May", "Jun", "Jul",
+        "Ago", "Sep", "Oct",
+        "Nov", "Dic"
+      ];
+
+      var day = date.getDate();
+      var monthIndex = date.getMonth();
+      var year = date.getFullYear();
+
+      return day + ' de ' + monthNames[monthIndex] + ', ' + year;
+    },
+
+    _formatDateWithoutYear: function(date) {
+      var date = this._formatDate(date);
+      return date.substring(0, date.length - 6);
+    },
+
+    _formatTime: function(date) {
+      var time = new Date(date);
+      var hr = time.getHours();
+      var min = time.getMinutes();
+
+      // add initial zero to minute
+      if (min < 10)
+          min = "0" + min;
+
+      // set AM or PM
+      var ampm = "am";
+      if( hr > 12 ) {
+          hr -= 12;
+          ampm = "pm";
+      }
+
+      return hr + ":" + min + ampm;
+    },
   }
 });
